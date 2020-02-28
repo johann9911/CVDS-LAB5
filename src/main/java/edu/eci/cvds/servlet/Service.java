e477b874a4ad02611415958c6060a5cb52b74dce
johann9911 <johann.bogota@mail.escuelaing.edu.co>
2020-02-27 21:33:45 -0500

    impl@Override

--- src/main/java/edu/eci/cvds/servlet/Service.java
+++ src/main/java/edu/eci/cvds/servlet/Service.java
@@ -52,6 +52,30 @@ public class Service extends HttpServlet{
       responseWriter.write("requerimiento inválido");
       }
   }
+  
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	 
+      Writer responseWriter = resp.getWriter();
+      try {
+          Optional <Integer> optId = Optional.ofNullable(Integer.parseInt(req.getParameter("id")));
+          Integer id = optId.isPresent() ? optId.get():1;
+          todos.add(getTodo(id));
+          resp.setStatus(HttpServletResponse.SC_OK);
+          responseWriter.write(todosToHTMLTable(todos));
+          responseWriter.flush();
+      }catch ( NumberFormatException e){
+          responseWriter.write("Requerimiento Inválido");
+          resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      }catch (FileNotFoundException e){
+          responseWriter.write("No encontrado.");
+          resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      }catch (MalformedURLException e){
+          responseWriter.write("Error interno en el Servidor ");
+      }catch (Exception e){
+      responseWriter.write("requerimiento inválido");
+      }
+  }
 
    public static Todo getTodo(int id) throws MalformedURLException, IOException {
        URL urldemo = new URL("https://jsonplaceholder.typicode.com/todos/" + id);
