973d7b0de42b195e37c94a4ff31efa053badcf79
johan stiven guerrero pineda <johanguerrero@MacBook-Air-de-johan.local>
2020-02-28 22:33:16 -0500

    Implementacion clase Bean

---
+++ src/main/java/edu/eci/cvds/servlet/model/Bean.java
@@ -0,0 +1,70 @@
+package edu.eci.cvds.servlet.model;
+
+import javax.faces.bean.ManagedBean;
+
+import java.util.Random;
+
+import javax.faces.bean.ApplicationScoped;
+
+@ApplicationScoped
+@ManagedBean(name = "guessBean")
+public class Bean {
+
+	private int numero;
+	private int numeroIntentos;
+	private int premio;
+	private String estado;
+	
+	public Bean() {
+		restart();
+	}
+	
+	public void guess(int valor) {
+		numeroIntentos+=1;
+		if(valor==numero){
+			estado="gano con premio de" + premio;
+		}else {
+			premio-=10000;
+		}
+	}
+	
+	public void restart() {
+		Random r=new Random();
+		numero=r.nextInt(10);
+		numeroIntentos=0;
+		premio=100000;
+		estado="No ha ganado";
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getNumeroIntentos() {
+		return numeroIntentos;
+	}
+
+	public void setNumeroIntentos(int numeroIntentos) {
+		this.numeroIntentos = numeroIntentos;
+	}
+
+	public int getPremio() {
+		return premio;
+	}
+
+	public void setPremio(int premio) {
+		this.premio = premio;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+}
